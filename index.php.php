c0a470678bcb6c128214e322aa21734323e17152
heroku121 <130366513+heroku121@users.noreply.github.com>
2023-07-18 20:06:06 +0530

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,223 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.8.14, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.14, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/redboost-128x72.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>RedBoost</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="menu cid-s48OLK6784" once="menu" id="menu1-h">
+    
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="container">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://bestofferhere.link/redboost">
+                        <img src="assets/images/redboost-216x122.png" alt="Mobirise Website Builder" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-primary display-7" href="https://bestofferhere.link/redboost">RedBoost</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black display-4" href="">Home</a></li>
+                    <li class="nav-item"><a class="nav-link link text-black display-4" href="">Contact</a></li></ul>
+                
+                
+            </div>
+        </div>
+    </nav>
+
+</section>
+
+<section data-bs-version="5.1" class="features16 cid-tKguVOdj8I" id="features17-10">
+    
+
+    
+    
+    <div class="container">
+        <div class="content-wrapper">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6">
+                    <div class="image-wrapper">
+                        <a href="https://bestofferhere.link/redboost"><img src="assets/images/redboost-666x375.png" alt="Mobirise Website Builder"></a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg">
+                    <div class="text-wrapper">
+                        <h6 class="card-title mbr-fonts-style display-5">
+                            <strong>RedBoost® Official</strong></h6>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-4"><strong>Red Boost</strong>: The Most Potent, Fast-Acting Formula For Increasing Male Sexual Performance.
+<br>Red Boost’s <strong>Unique Combination</strong> of Nutrients Combine To Create a Potent Synergistic Effect.</p>
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-primary display-4" href="https://bestofferhere.link/redboost">Order Now</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="content15 cid-tJGzKBmeqj" id="content15-v">
+
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-md-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-left">
+                        <h4 class="card-title mbr-fonts-style mbr-white mb-3 display-5"><strong>100% Satisfaction
+</strong><div><strong>180-Day Money Back Guarantee</strong></div></h4>
+                        <p class="mbr-text mbr-fonts-style display-7">Your order today is covered by our iron-clad 180-day 100% money-back guarantee. If you are not impressed with the results, then at any time in the next 180 days write to us and we’ll refund every single cent.<br></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="content11 cid-tJGI9Bw5Sn" id="content11-w">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <div class="mbr-section-btn align-center"><a class="btn btn-primary display-4" href="https://bestofferhere.link/redboost">Buy Now</a>
+                    <a class="btn btn-primary-outline display-4" href="https://bestofferhere.link/redboost">Official Website</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="pricing2 cid-tJGOxACajd" id="pricing2-y">
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>30 Day Supply</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$69</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>Per Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">Before Price : $297</li><li class="list-group-item">Now : $69</li><li class="list-group-item">-</li><li class="list-group-item">+ SHIPPING</li><br><li class="list-group-item">180 days Guarantee</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/redboost" class="btn btn-primary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>180 Day Supply</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$39</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>Per Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">Before Price : $1782</li><li class="list-group-item">Now : $234</li><li class="list-group-item">2 Free Bonus</li><li class="list-group-item">Free Shipping</li><br><li class="list-group-item">180 days Guarantee</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/redboost" class="btn btn-primary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5"><strong>90 Day Supply</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$59</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>Per Bottle</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">Before Price : $891</li><li class="list-group-item">Now : $177</li><li class="list-group-item">1 Free Bonus</li><li class="list-group-item">+ SHIPPING</li><br><li class="list-group-item">180 days Guarantee</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/redboost" class="btn btn-primary display-4">Buy Now</a></div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-white">Privacy</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-white">Terms of Use</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-white">Disclaimer</a></li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Copyright 2020 RedBoost. All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+  
+  
+</body>
+</html>
+';
+?>
